875bdb59ce1c085c110ff4719f37d77d4e854728
Veerababu <149765468+Cveerababu15@users.noreply.github.com>
2025-12-11 09:02:23 +0530

    Remove socket.io import from server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const http = require('http');
-const { Server } = require('socket.io');
 const { addmessage } = require("./controllers/messagecontroller.js");
 
 dotenv.config();
